4cc3ab1952bae4226a438ef7d124c9b0122357e9
ZackFox <firstthinker1989@gmail.com>
2019-07-09 14:30:38 +0500

    resume item style

--- src/components/ResumeListItem/ResumeListItem.tsx
+++ src/components/ResumeListItem/ResumeListItem.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Resume from "../../store/resume/types";
-import { numToAge } from "../../helpers/numToAge";
+import Resume from "../../store/types";
+import { numToWord } from "../../helpers/numToWord";
 
 import "./ResumeListItem.css";
+import getExperienceTerm from "../../helpers/getExperienceTerm";
 
 interface ResumeListItemProps {
   resume: Resume;
@@ -19,15 +20,30 @@ const ResumeListItem: React.FunctionComponent<ResumeListItemProps> = ({
       to={`/resume/${resume.id}`}
       key={resume.id}
     >
-      <div className="list-item-picture">
+      <div className="picture">
         <img src={resume.photo} alt="" />
       </div>
-      <div className="list-item-info">
-        <h3>{resume.position}</h3>
-        <div className="user-info">
-          <span>{`${resume.firstname} ${resume.lastname}, 
-            ${resume.sex}, ${numToAge(resume.age, ["год", "года", "лет"])},
-            ${resume.city}`}</span>
+      <div className="person-container">
+        <div className="p-left">
+          <div className="person-info">
+            <h4 className="person-name">
+              {`${resume.firstname} ${resume.lastname}`}
+            </h4>
+            <span className="person-details">{`
+                ${resume.gender}, ${numToWord(resume.age)}, 
+                г.${resume.city}
+              `}</span>
+          </div>
+          <div className="full-experience">
+            <span>Опыт работы: </span>
+            <span className="exp-term">
+              {getExperienceTerm(resume.experience)}
+            </span>
+          </div>
+        </div>
+        <div className="p-right">
+          <h3 className="position">{resume.position}</h3>
+          <div className="salary">30 000 Руб.</div>
         </div>
       </div>
     </Link>
