647da0c5d7d159aee76ce7c1929db286a01e2bad
ZackFox <firstthinker1989@gmail.com>
2019-08-03 18:50:21 +0500

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import store from "./store";
-import * as serviceWorker from "./serviceWorker";
+import store from './store';
 
-import App from "./containers/App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,7 +14,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root"),
+  document.getElementById('root'),
 );
-
-serviceWorker.unregister();
