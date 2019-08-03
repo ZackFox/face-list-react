4fb6e12cf7b3e6c3db37a9a050e88360108abccb
ZackFox <firstthinker1989@gmail.com>
2019-08-03 18:49:06 +0500

    nullable date

--- src/helpers/getExperienceTerm.tsx
+++ src/helpers/getExperienceTerm.tsx
@@ -1,12 +1,12 @@
-import { ExpItem } from "../store/types";
+import { ExperienceItem } from "../store/types";
 import { numToWord } from "./numToWord";
 
-const getExperienceTerm = (data: ExpItem[]) => {
+const getExperienceTerm = (data: ExperienceItem[]) => {
   if (!data.length) return "нет";
 
   let total = data.reduce((count, item) => {
     const msStart = Date.parse(item.dateStart);
-    const msEnd = Date.parse(item.dateEnd);
+    const msEnd = item.dateEnd ? Date.parse(item.dateEnd) : Date.now();
     count += msEnd - msStart;
     return count;
   }, 0);
@@ -19,7 +19,7 @@ const getExperienceTerm = (data: ExpItem[]) => {
   const years = Math.floor(days / 365);
 
   const Y = years ? numToWord(years) : "";
-  const M = numToWord(months, ["месяц", "месяца", "месяцев"]);
+  const M = months ? numToWord(months, ["месяц", "месяца", "месяцев"]) : "";
   return `${Y} ${M}`;
 };
 
