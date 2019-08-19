d6cd927ce5e83f2674d8a6e88cb4e56407c14e40
ZackFox <firstthinker1989@gmail.com>
2019-08-19 17:25:01 +0500

    index

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,62 @@
-import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import Header from './components/Header/Header';
-import HomePage from './pages/HomePage';
-import ResumePage from './pages/ResumePage';
-import CreateResume from './pages/CreateResume';
-import { connect } from 'react-redux';
-import { AppState } from './store';
-import { getUser } from './actions/userActions';
-import LoadingBox from './components/LoadingBox/LoadingBox';
+import { AppState } from "./store";
 
-const App: React.FunctionComponent<any> = props => {
-  const { isLogined, loading, getUser } = props;
+import Header from "./components/Header/Header";
+import Registration from "./pages/Registration";
+import HomePage from "./pages/HomePage";
+import ResumePage from "./pages/ResumePage";
+import CreateResume from "./pages/CreateResume";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
+import { getUser } from "./actions/userActions";
+import { User } from "./store/types";
+
+interface AppProps {
+  isLoggedIn: boolean;
+  user: User | null;
+  getUser: () => void;
+}
+
+const App: React.FunctionComponent<AppProps> = ({
+  isLoggedIn,
+  getUser,
+  user,
+}) => {
   useEffect(() => {
-    if (isLogined) {
+    if (isLoggedIn && !user) {
       getUser();
     }
-  }, [isLogined]);
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
       <Header />
       <main className="app-main">
-        {loading && <LoadingBox />}
-        <div className="wrapper">
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/page/:numPage" component={HomePage} />
-            <Route path="/resume/:id" component={ResumePage} />
-            <Route path="/create" component={CreateResume} />
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/page/:numPage" component={HomePage} />
+          <Route path="/resume/:id" component={ResumePage} />
+          <ProtectedRoute
+            path="/create"
+            isLoggedIn={isLoggedIn}
+            component={CreateResume}
+            redirectTo={"/page/1"}
+          />
+          <ProtectedRoute
+            path="/signup"
+            isLoggedIn={!isLoggedIn}
+            component={Registration}
+            redirectTo={"/page/1"}
+          />
+        </Switch>
       </main>
       <footer>
-        <div className="wrapper">футер</div>
+        <div className="wrapper">
+          <a href="https://github.com/ZackFox/face-list-react">source code</a>
+        </div>
       </footer>
     </div>
   );
@@ -42,8 +64,8 @@ const App: React.FunctionComponent<any> = props => {
 
 export default connect(
   (state: AppState) => ({
-    isLogined: state.user.isLogined,
-    loading: state.resume.loading,
+    isLoggedIn: state.user.isLoggedIn,
+    user: state.user.data,
   }),
   { getUser },
 )(App);
