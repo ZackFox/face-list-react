1007836c8933373cce525a088eb341189fcb8209
ZackFox <firstthinker1989@gmail.com>
2019-08-19 17:17:17 +0500

    api logout

--- src/actions/apiClient.ts
+++ src/actions/apiClient.ts
@@ -1,19 +1,29 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import cookies from 'js-cookie';
+import { createBrowserHistory } from "history";
+import axios, { AxiosRequestConfig } from "axios";
+import cookies from "js-cookie";
+import store from "../store";
+import { LOGOUT, logout } from "./userActions";
 
 const apiClient = axios.create();
 
 apiClient.interceptors.request.use(
   (config: AxiosRequestConfig) => {
-    const token = cookies.get('token');
+    const token = cookies.get("token");
     if (token) {
       config.headers.authorization = `Bearer ${token}`;
     }
     return config;
   },
+  err => Promise.reject(err),
+);
+
+apiClient.interceptors.response.use(
+  response => response,
   err => {
-    cookies.remove('token');
-    return Promise.reject(err);
+    if (err.response && err.response.status === 401) {
+      logout()(store.dispatch);
+    }
+    return Promise.reject(err.response);
   },
 );
 
